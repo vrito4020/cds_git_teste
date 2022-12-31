e06e9d7c9ab61a6dfcf623df95d92284610f27a1
Victor <victor.giorgi8@gmail.com>
2022-12-31 18:01:42 -0300

    adicionando funcao gather_data

---
+++ teste.py
@@ -0,0 +1,11 @@
+def gather_data():
+    n1 = int(input('Primeiro valor:'))
+    n2 = int(input('Segundo valor:'))
+    return n1, n2
+
+def maine():
+    print(2*2)
+    return None
+
+if __name__ == '__main__':
+    maine()
