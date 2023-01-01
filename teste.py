d232a6afcfd1c4e4f33dd0eea0541035a5e59a39
Victor <victor.giorgi8@gmail.com>
2022-12-31 23:21:11 -0300

    alterado a maneira de recuperar operacao

--- teste.py
+++ teste.py
@@ -1,7 +1,11 @@
+def get_operation():
+    op = input('operacao:')
+    return op
+
 def gather_data():
     n1 = input('Primeiro valor:')
     n2 = input('Segundo valor:')
-    op = input('operacao:')
+    op = get_operation()
     return n1, n2, op
 
 def maine(n1, n2, op):
